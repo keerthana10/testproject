ae43f85698c74072eb5068e5e864c0b04ff1ee8e
keerthana10 <Admin@DESKTOP-24NS24I.blr-ad2.systech.sysfore.com>
2017-05-09 13:03:29 +0530

    first commit

---
+++ ProudctsApp/ProudctsApp/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using ProudctsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProudctsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+           {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+           };
+        
+        [HttpGet]
+        [Route("api/Products/GetAllProducts")]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+     
+        [HttpGet]
+        [Route("api/Products/GetProduct/{id}")]
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
+//http://stackoverflow.com/questions/36557911/routeprefix-vs-route
+
+
